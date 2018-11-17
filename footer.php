7d3f166234a467a4c85e8b2b4c5f2544e16c93b2
Andi North <andi@mangopear.co.uk>
2018-11-17 10:26:41 +0000

    [Global] Init basic footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Template partial: Footer
+	 *
+	 * @category 	Templates
+	 * @package  	mangopear-core
+	 * @author  	Andi North <andi@mangopear.co.uk>
+	 * @copyright  	2018 Mangopear creative
+	 * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+	 * @version  	1.0.0
+	 * @since   	1.0.0
+	 */
+	
+?>
+
+
+	<?php wp_footer(); ?>
+
+
+</body>
+</html>
