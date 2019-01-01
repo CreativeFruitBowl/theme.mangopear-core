da59b9434f24e3e21ace04ce2658d3997ace7cc2
Andi North <andi@mangopear.co.uk>
2019-01-01 13:06:58 +0000

    [Global] Footer: Added footer partial

---
+++ template-partials/footer-additions.php
@@ -0,0 +1,17 @@
+<?php
+
+	/**
+	 * [Partial] Footer additions
+	 *
+	 * @category 	Core WordPress template files
+	 * @package  	mangopear-core
+	 * @author  	Andi North <andi@mangopear.co.uk>
+	 * @copyright  	2018 Mangopear creative
+	 * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+	 * @version  	1.0.0
+	 * @since   	1.0.0
+	 */
+	
+?>
+
+	
