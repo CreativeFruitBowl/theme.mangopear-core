00940dcf8825cb75c29b8d567c47daa58b5e254b
Andi North <andi@mangopear.co.uk>
2019-03-04 14:06:58 +0000

    [Global] Favicons: Moved favicons to separate file - this is for easier manipulation in child themes.

--- header.php
+++ header.php
@@ -41,18 +41,7 @@
 
 
 		<!-- Favicon -->
-		<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/favicon.ico">
-		<link rel="icon"          type="image/png"    href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/favicon-32x32.png"             sizes="32x32">
-		<link rel="icon"          type="image/png"    href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/favicon-16x16.png"             sizes="16x16">
-		<link rel="apple-touch-icon"                  href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/apple-touch-icon.png"          sizes="180x180">
-		<link rel="mask-icon"                         href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/safari-pinned-tab.svg?v=1.0.0" color="<?php echo BROWSER_TAB_COLOUR; ?>">
-		<link rel="manifest"                          href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/site.webmanifest">
-		<meta name="theme-color"                      content="<?php echo BROWSER_TAB_COLOUR; ?>">
-		<meta name="msapplication-TileColor"          content="<?php echo BROWSER_TAB_COLOUR; ?>">
-		<meta name="msapplication-TileImage"          content="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/apple-touch-icon-144x144.png">
-		<meta name="msapplication-config"             content="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/browserconfig.xml">
-		<meta name="apple-mobile-web-app-title"       content="Mangopear creative">
-		<meta name="application-name"                 content="Mangopear creative">
+		<?php echo get_template_part('template-partials/favicons'); ?>
 
 
 		<?php wp_head(); ?>
---
+++ template-partials/favicons.php
@@ -0,0 +1,28 @@
+<?php
+
+	/**
+	 * [Partial] Favicons
+	 *
+	 * @category 	Core WordPress template files
+	 * @package  	mangopear-core
+	 * @author  	Andi North <andi@mangopear.co.uk>
+	 * @copyright  	2018 Mangopear creative
+	 * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
+	 * @version  	1.0.0
+	 * @since   	1.0.0
+	 */
+	
+?>
+
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/favicon.ico">
+	<link rel="icon"          type="image/png"    href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/favicon-32x32.png"             sizes="32x32">
+	<link rel="icon"          type="image/png"    href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/favicon-16x16.png"             sizes="16x16">
+	<link rel="apple-touch-icon"                  href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/apple-touch-icon.png"          sizes="180x180">
+	<link rel="mask-icon"                         href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/safari-pinned-tab.svg?v=1.0.0" color="<?php echo BROWSER_TAB_COLOUR; ?>">
+	<link rel="manifest"                          href="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/site.webmanifest">
+	<meta name="theme-color"                      content="<?php echo BROWSER_TAB_COLOUR; ?>">
+	<meta name="msapplication-TileColor"          content="<?php echo BROWSER_TAB_COLOUR; ?>">
+	<meta name="msapplication-TileImage"          content="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/apple-touch-icon-144x144.png">
+	<meta name="msapplication-config"             content="<?php echo get_template_directory_uri(); ?>/resources/images/favicon/browserconfig.xml">
+	<meta name="apple-mobile-web-app-title"       content="Mangopear creative">
+	<meta name="application-name"                 content="Mangopear creative">
