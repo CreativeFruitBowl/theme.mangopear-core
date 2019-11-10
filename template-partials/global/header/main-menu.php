37d298c7bd8394f1fde79569782e47786a5e62b5
Andi North <andi@mangopear.co.uk>
2019-11-10 20:33:57 +0000

    [Header] Rolled out new mega nav formatting

--- template-partials/global/header/main-menu.php
+++ template-partials/global/header/main-menu.php
@@ -14,67 +14,93 @@
 	
 ?>
 
-	<nav class="c-head-navigation__menu">
-		<button class="o-button  o-button--secondary  c-head__button  c-head__button--main-menu  js-head-navigation__menu__launcher" type="button">
+	<div class="c-mega-nav">
+		<button class="o-button  o-button--secondary  c-mega-nav__button  js-head-navigation__menu__launcher" type="button">
 			<svg class="o-button__icon  o-button__icon--left" height="22" width="22" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#navigation"/></svg>
 			<span class="o-button__text">Menu</span>
 		</button>
 
 
-		<div class="c-head-navigation__menu__wrap  js-head-navigation__menu__wrap  is-hidden">
-			<div class="o-nav  o-nav--row">
-				<ul class="o-nav__list">
-					<li class="o-nav__item  o-nav__item--top-level  o-nav__item--this-site">
-						<strong class="o-nav__text  o-nav__text--top-level">Mangopear creative:</strong>
+		<div class="c-mega-nav__wrap  js-head-navigation__menu__wrap  is-hidden">
+			<div class="c-mega-nav__overflow">
+				<div class="c-mega-nav__section  c-mega-nav__section--primary">
+					<nav class="o-nav  o-nav--main  o-nav--main-primary">
+						<header class="c-mega-nav__header">
+							<svg class="c-mega-nav__header__icon" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" xml:space="preserve"><circle cx="55" cy="50" r="50" fill="#499e00"/><path fill="#fff" d="M83.9 28.2L59.5 14.1a8.85 8.85 0 00-8.9 0L26.1 28.2a8.95 8.95 0 00-4.5 7.7v28.2c0 3.2 1.7 6.2 4.5 7.7l24.4 14.1c1.4.8 2.9 1.2 4.5 1.2 1.5 0 3.1-.4 4.5-1.2l24.4-14.1c2.8-1.6 4.5-4.6 4.5-7.7V35.9c0-3.2-1.7-6.2-4.5-7.7zM85 64.1c0 2-1.1 3.8-2.8 4.8L57.8 83c-1.7 1-3.9 1-5.6 0L27.8 68.9c-1.7-1-2.8-2.8-2.8-4.8V35.9c0-2 1.1-3.8 2.8-4.8L52.2 17c.9-.5 1.8-.7 2.8-.7 1 0 1.9.2 2.8.7l24.4 14.1c1.7 1 2.8 2.8 2.8 4.8v28.2z"/><path fill="#fff" d="M64.2 38.5c-3.3 0-5.5.9-7.9 3-1.7-2-4.1-3-6.9-3-2.4 0-4 .7-5.5 2v-1.3h-7.5v22.2H44V47.2c1.1-1.4 2.2-1.9 3.3-1.9 2.4 0 4 1.5 4 4.7v11.5h7.4V48.8c0-.5 0-1.1-.1-1.6 1.1-1.3 2.2-1.8 3.4-1.8 2.4 0 4 1.5 4 4.7v11.5h7.4V48.8c0-6.4-3.9-10.3-9.2-10.3zM79.1 57.1c-1.3 0-2.3.9-2.3 2.2 0 1.3 1 2.2 2.3 2.2 1.2 0 2.3-.9 2.3-2.2 0-1.2-1.1-2.2-2.3-2.2z"/></svg>
+							<strong class="c-mega-nav__header__text">We are Mangopear</strong>
+						</header>
 
-						<ul class="o-nav__list  o-nav__list--second-level">
-							<li class="o-nav__item"><a href="https://mangopear.co.uk/"            class="o-nav__link">Home</a></li>
-							<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">What we do</a></li>
-							<li class="o-nav__item"><a href="https://mangopear.co.uk/our-work/"   class="o-nav__link">Our work</a></li>
-							<li class="o-nav__item"><a href="https://mangopear.co.uk/about/"      class="o-nav__link">About us</a></li>
-							<li class="o-nav__item"><a href="https://blog.mangopear.co.uk/"       class="o-nav__link">News &amp; Press</a></li>
-							<li class="o-nav__item"><a href="https://mangopear.co.uk/contact/"    class="o-nav__link">Contact us</a></li>
+
+						<div class="c-mega-nav__split  u-clearfix">
+							<ul class="o-nav__list">
+								<li class="o-nav__item"><span class="o-nav__text  o-nav__text--title">What we do</span></li>
+
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">Website design</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">E-commerce</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">Website optimisation</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">Design &amp; branding</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">Consultancy</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/what-we-do/" class="o-nav__link">Photography</a></li>
+							</ul>
+
+
+							<ul class="o-nav__list">
+								<li class="o-nav__item"><span class="o-nav__text  o-nav__text--title">About us</span></li>
+
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/our-work/" class="o-nav__link">Our work</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/about/"    class="o-nav__link">About us</a></li>
+								<li class="o-nav__item"><a href="https://mangopear.co.uk/contact/"  class="o-nav__link">Contact us</a></li>
+								<li class="o-nav__item"><a href="https://blog.mangopear.co.uk/"     class="o-nav__link">News &amp; press</a></li>
+							</ul>
+						</div><!-- /.c-mega-nav__split -->
+					</nav>
+				</div><!-- /.c-mega-nav__section -->
+
+
+
+
+
+				<div class="c-mega-nav__section  c-mega-nav__section--secondary">
+					<nav class="o-nav  o-nav--main  o-nav--main-secondary">
+						<header class="c-mega-nav__header">
+							<strong class="c-mega-nav__header__text">Also from us</strong>
+						</header>
+
+						<ul class="o-nav__list">
+							<li class="o-nav__item  c-mega-nav__bold">
+								<a href="https://scenicbuses.uk/" class="o-nav__link">
+									<svg class="c-mega-nav__bold__icon" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" xml:space="preserve"><circle cx="50" cy="49.9" r="49.5" fill="#b3d300"/><path d="M49.5 70.7c13.7.9 39.2-18.3 39.2-18.3s-6.9-2.7-14.5-10.1C67.1 35.4 60 29.4 52.4 28.9c-11.7-.8-22 5.4-25.2 14.4 9.6-1.4 32.4 7.1 32.4 7.1-18.4-4.4-30-4.7-48.2-2.8l2.4 4.8s7.8-1.7 12.5-1.3c1 10.2 10.8 18.7 23.2 19.6z" fill="#fff"/></svg>
+									<strong class="c-mega-nav__bold__title">Scenic Buses</strong><span class="u-hide"> - </span><span class="c-mega-nav__bold__description">showcasing the UK's most scenic bus routes</span>
+								</a>
+							</li>
+
+
+							<li class="o-nav__item  c-mega-nav__bold">
+								<a href="https://witterings.mangopear.co.uk/" class="o-nav__link">
+									<svg class="c-mega-nav__bold__icon" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" xml:space="preserve"><circle cx="50" cy="49.9" r="49.5" fill="#f2c500"/><path d="M25.4 63.8c0 2.7 1.2 5.1 3.1 6.8v3.9c0 2.6 2.1 4.6 4.6 4.6 2.6 0 4.6-2.1 4.6-4.6V73h24.6v1.5c0 2.5 2.1 4.6 4.6 4.6 2.5 0 4.6-2.1 4.6-4.6v-3.9c1.9-1.7 3.1-4.1 3.1-6.8V33c0-10.8-11-12.3-24.6-12.3S25.4 22.2 25.4 33v30.8zm10.7 3.1c-2.6 0-4.6-2.1-4.6-4.6 0-2.6 2.1-4.6 4.6-4.6s4.6 2.1 4.6 4.6c.1 2.5-2 4.6-4.6 4.6zm27.8 0c-2.6 0-4.6-2.1-4.6-4.6 0-2.6 2.1-4.6 4.6-4.6s4.6 2.1 4.6 4.6c0 2.5-2.1 4.6-4.6 4.6zm4.6-18.5h-37V33h37v15.4z" fill="#fff"/></svg>
+									<strong class="c-mega-nav__bold__title">Witterings</strong><span class="u-hide"> - </span><span class="c-mega-nav__bold__description">our witterings about public transport</span>
+								</a>
+							</li>
+
+
+
+							<li class="o-nav__item  c-mega-nav__bold">
+								<a href="https://coding.mangopear.co.uk/" class="o-nav__link">
+									<svg class="c-mega-nav__bold__icon" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" xml:space="preserve"><circle cx="50" cy="50" r="50" fill="#f27500"/><path d="M42.4 66.5L25.8 50l16.5-16.5c1.7-1.7 1.7-4.3 0-5.9a4.05 4.05 0 00-5.9 0L17 47a4.17 4.17 0 000 6l19.5 19.5c1.7 1.7 4.3 1.7 5.9 0 1.6-1.7 1.6-4.3 0-6zm15.2 0L74.2 50 57.6 33.5a4.05 4.05 0 010-5.9c1.7-1.7 4.3-1.7 5.9 0L83 47c1.7 1.7 1.7 4.3 0 6L63.6 72.5a4.05 4.05 0 01-5.9 0 4.26 4.26 0 01-.1-6z" fill="#fff"/></svg>
+									<strong class="c-mega-nav__bold__title">Coding</strong><span class="u-hide"> - </span><span class="c-mega-nav__bold__description">our blog about design &amp; development</span>
+								</a>
+							</li>
+
+
+							<li class="o-nav__item  c-mega-nav__bold">
+								<a href="https://account.mangopear.co.uk/" class="o-nav__link">
+									<svg class="c-mega-nav__bold__icon" height="24" width="24" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" xml:space="preserve"><circle cx="50" cy="49.9" r="49.5" fill="#1da1f2"/><path d="M50 49.9c7.1 0 12.9-5.8 12.9-12.9S57.1 24.2 50 24.2 37.1 30 37.1 37.1 42.9 49.9 50 49.9zm0 6.4c-8.6 0-25.7 4.3-25.7 12.9v3.2c0 1.8 1.4 3.2 3.2 3.2h45c1.8 0 3.2-1.4 3.2-3.2v-3.2c0-8.6-17.1-12.9-25.7-12.9z" fill="#fff"/></svg>
+									<strong class="c-mega-nav__bold__title">Client dashboard</strong><span class="u-hide"> - </span><span class="c-mega-nav__bold__description">log in to view invoices &amp; your retainer</span>
+								</a>
+							</li>
 						</ul>
-					</li>
-
-
-					<li class="o-nav__item  o-nav__item--top-level" data-nav="also-from-us">
-						<strong class="o-nav__text  o-nav__text--top-level">Also from us:</strong>
-
-						<ul class="o-nav__list  o-nav__list--second-level">
-							<li class="o-nav__item">
-								<a href="https://mangopear.co.uk/" class="o-button  o-nav__link">
-									<svg class="o-button__icon  o-button__icon--left" height="44" width="44" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#nav-icon--mangopear-creative"/></svg>
-									<span class="o-button__text"><strong>Mangopear creative</strong> <span class="u-hide">- </span>visit our main website to see what we do.</span>
-								</a>
-							</li>
-
-
-							<li class="o-nav__item">
-								<a href="https://witterings.mangopear.co.uk/" class="o-button  o-nav__link">
-									<svg class="o-button__icon  o-button__icon--left" height="44" width="44" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#nav-icon--witterings"/></svg>
-									<span class="o-button__text"><strong>Witterings from Mangopear</strong> <span class="u-hide">- </span>our witterings about public transport.</span>
-								</a>
-							</li>
-
-
-							<li class="o-nav__item">
-								<a href="https://coding.mangopear.co.uk/" class="o-button  o-nav__link">
-									<svg class="o-button__icon  o-button__icon--left" height="44" width="44" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#nav-icon--coding"/></svg>
-									<span class="o-button__text"><strong>Coding from Mangopear</strong> <span class="u-hide">- </span>our blog about design &amp; web development.</span>
-								</a>
-							</li>
-
-
-							<li class="o-nav__item">
-								<a href="https://account.mangopear.co.uk/" class="o-button  o-nav__link">
-									<svg class="o-button__icon  o-button__icon--left" height="44" width="44" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#nav-icon--account"/></svg>
-									<span class="o-button__text"><strong>Your account</strong> <span class="u-hide">- </span>log in to view invoices &amp; your retainer.</span>
-								</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</div><!-- /.o-nav -->
-		</div><!-- /.c-head-navigation__menu__wrap -->
-	</nav>
+					</nav>
+				</div><!-- /.c-mega-nav__section -->
+			</div><!-- /.c-mega-nav__overflow -->
+		</div><!-- /.c-mega-nav__wrap -->
+	</div><!-- /.c-mega-nav -->
