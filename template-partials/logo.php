b9bd118c6d3a9abf2b9b3d0532c608295c063f4d
Andi North <andi@mangopear.co.uk>
2019-12-20 23:34:53 +0000

    [Global] Amended logo to work better on responsive views.

--- template-partials/logo.php
+++ template-partials/logo.php
@@ -15,6 +15,7 @@
 ?>
 
 	<a href="<?php echo get_site_url(); ?>" class="c-head-navigation__logo">
-		<svg class="c-head-navigation__logo__icon" height="75" width="175" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#mangopear-logo--white"/></svg>
+		<svg class="c-head-navigation__logo__icon  u-portable--hide" height="75" width="175" role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#mangopear-logo--white"/></svg>
+		<svg class="c-head-navigation__logo__icon  u-desk--hide" height="75" width="75"  role="presentation"><use xlink:href="<?php echo MANGOPEAR_SPRITE; ?>#mangopear-icon--white"/></svg>
 		<span class="u-invisible">We are Mangopear creative.</span>
 	</a>
